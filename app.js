e8ca7b2d2d0dc4adafd63bf0f55b777f5bfe350e
Irina Volkhonskaya <irina.volkhonskaya@IMPRIVATA.com>
2024-08-12 06:01:44 +0200

    create a backend variables before deployment

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://dg789hngm457kjklZUnbmg8:dg789hngm457kjklZUnbmg8@cluster0.81fxn4w.mongodb.net/places?retryWrites=true&w=majority&appName=Cluster0"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.81fxn4w.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
     console.log("Connected to MongoDB");
